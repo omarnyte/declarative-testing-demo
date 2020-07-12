dc6368179adcb5a0f431804d663ff1059bb28006
Omar De Los Santos <omarnyte@gmail.com>
2020-07-12 13:25:36 -0700

    Display temperature based on location

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CELCIUS = "celcius";
+const FAHRENHEIT = "fahrenheit";
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      unit: "fahrenheit", 
+      weather: null 
+    }
+  }
+
+  getForecastForCoordinates(lat, long) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({ weather: data.main })
+      });
+  }
+  
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      this.getForecastForCoordinates(coords.latitude, coords.longitude)
+    })
+  }
+
+  convertKelvinToUnit(kelvin, unit) {
+    return Math.round(9/5 * (kelvin - 273) + 32)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.weather && this.convertKelvinToUnit(this.state.weather.temp, this.state.unit)}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
